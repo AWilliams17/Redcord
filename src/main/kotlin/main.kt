8c4981a2cbc098890ed3c2aed0f9cd383f05ffd3
Austin Williams <awilliams17412@gmail.com>
2020-07-27 15:53:13 -0500

    Bot testing: Join server and test listener

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,17 @@
 package com.github.awilliams17.redcord
+import org.javacord.api.DiscordApi
+import org.javacord.api.DiscordApiBuilder
 
 
 fun main(args : Array<String>) {
+    val token = args[0]
+    val discordApi = DiscordApiBuilder().setToken(token).login().join()
+
+    discordApi.addMessageCreateListener { event ->
+        if (event.message.content.equals("test", ignoreCase = true)) {
+            event.channel.sendMessage("test.")
+        }
+    }
     
+    println(discordApi.createBotInvite())
 }
