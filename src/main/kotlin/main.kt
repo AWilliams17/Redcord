d1390313a816eb15f9e799b3bfb16238064a934c
Austin Williams <awilliams17412@gmail.com>
2020-07-29 16:03:12 -0500

    Added emojiparser dependency

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 package com.github.awilliams17.redcord
 import org.javacord.api.DiscordApi
 import org.javacord.api.DiscordApiBuilder
+import com.vdurmont.emoji.EmojiParser
 
 
 fun main(args : Array<String>) {
@@ -8,9 +9,9 @@ fun main(args : Array<String>) {
     val discordApi = DiscordApiBuilder().setToken(token).login().join()
 
     discordApi.addMessageCreateListener { event ->
-        if (event.message.content.equals("test", ignoreCase = true)) {
-            event.channel.sendMessage("test.")
-        }
+        //if (event.message.content.equals("test", ignoreCase = true)) {
+        //    event.channel.sendMessage("test.")
+        //}
     }
     
     println(discordApi.createBotInvite())
